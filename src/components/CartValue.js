da9546675478bd79acfaceaf88a421aa8b3b9c5d
Wai Yan <waiyankyawsdk1999@gmail.com>
2024-03-09 22:46:33 +0630

    feat: budget app

--- src/components/CartValue.js
+++ src/components/CartValue.js
@@ -2,6 +2,16 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const CartValue = () => {
+    const {expenses, location} = useContext(AppContext);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
+    return (
+        <div className="alert alert-primary">
+            <span>CartValue: {location}</span>
+            <span className="badge badge-secondary">Total Expenses: ${totalExpenses}</span>
+        </div>
+    );
 };
 
 export default CartValue;
